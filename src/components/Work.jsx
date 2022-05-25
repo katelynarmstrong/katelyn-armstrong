a1068af942c8fa70ace4bde037119ac5cd88eb86
katelynarmstrong <katelynarmstrong6@gmail.com>
2022-05-25 16:39:53 -0500

    taco bell throwdown added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,6 +5,7 @@ import codeQuiz from '../assets/codeQuiz.png';
 import dreamTeam from '../assets/dreamTeam.png';
 import weatherDashboard from '../assets/weatherDashboard.png';
 import letsBudget from '../assets/letsBudget.png'
+import tacoBellThrowdown from '../assets/tacoBellThrowdown.png'
 
 const Work = () => {
   return (
@@ -140,21 +141,21 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${letsBudget})` }}
+            style={{ backgroundImage: `url(${tacoBellThrowdown})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                PWA using MongoDB
+                MERN Stack Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://lets-budget.herokuapp.com/' target={'_blank'}>
+                <a href='https://taco-bell-throwdown.herokuapp.com/' target={'_blank'}>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/katelynarmstrong/lets-budget' target={'_blank'}>
+                <a href='https://github.com/joshlyons225/taco-bell-throwdown' target={'_blank'}>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
